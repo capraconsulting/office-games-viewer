8f4680781a36cb7be349e4d6d25ade7cd7b3290a
Orhan Hirsch <orhanhenrik@gmail.com>
2017-07-06 22:08:11 +0200

    untested MVP

--- src/App.js
+++ src/App.js
@@ -3,16 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      currentSession: {}
+    };
+  }
+
+  componentDidMount() {
+    this.props.rebase.bindToState('cards', {
+      context: this,
+      state: 'cards',
+      asArray: true
+    });
+    this.props.rebase.bindToState('current_session', {
+      context: this,
+      state: 'currentSession'
+    })
+  }
+
   render() {
+    console.log(this.state.cards);
+    console.log(this.state.currentSession);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          {this.state.currentSession.session_started ?
+            "Game in progress" : "Waiting for players.."
+          }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          Player 1: {
+            (this.state.currentSession.players &&
+            this.state.currentSession.players.length > 0) ?
+            this.state.currentSession.players[0].slack_first_name :
+            "Not registered"
+          }
+        </div>
+        <div>
+          Player 2: {
+            (this.state.currentSession.players &&
+            this.state.currentSession.players.length > 1) ?
+            this.state.currentSession.players[1].slack_first_name :
+            "Not registered"
+          }
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Rebase from 're-base';
+import firebase from 'firebase/app';
+import database from 'firebase/database';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyCBww2YLzI5jScmJarlxKCQguo2-DDtxNI",
+  authDomain: "capra-office-games.firebaseapp.com",
+  databaseURL: "https://capra-office-games.firebaseio.com",
+  storageBucket: "capra-office-games.appspot.com",
+  // messagingSenderId: "webapp",
+});
+const db = database(app);
+const rebase = Rebase.createClass(db);
+
+
+
+ReactDOM.render(<App rebase={rebase} />, document.getElementById('root'));
